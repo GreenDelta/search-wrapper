1f5918af253d7a6202edd5f500937a05a9eceebf
greve <greve@greendelta.com>
2017-10-06 16:03:13 +0200

    Added bulk functions

--- src/main/java/com/greendelta/lca/search/SearchClient.java
+++ src/main/java/com/greendelta/lca/search/SearchClient.java
@@ -7,17 +7,27 @@ import java.util.Set;
 public interface SearchClient {
 
 	SearchResult search(SearchQuery searchQuery);
-	
+
 	void create(Map<String, Object> settings);
-	
+
 	void index(String type, String id, Map<String, Object> content);
+
+	void index(String type, Map<String, Map<String, Object>> contentsById);
+
+	void index(Map<String, Map<String, Map<String, Object>>> contentsByIdByType);
 
 	void remove(String type, String id);
 
+	void remove(String type, Set<String> ids);
+
+	void remove(Map<String, Set<String>> idsByType);
+
 	Map<String, Object> get(String type, String id);
-	
+
 	List<Map<String, Object>> get(String type, Set<String> ids);
 
+	List<Map<String, Object>> get(Map<String, Set<String>> idsByType);
+
 	void delete();
-	
+
 }
