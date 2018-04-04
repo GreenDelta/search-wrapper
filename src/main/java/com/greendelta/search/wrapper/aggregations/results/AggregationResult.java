fb0b93d13249648dcdec4f3a1e5fdbce63381dc7
greve <greve@greendelta.com>
2018-04-04 17:29:18 +0200

    Refactored aggregation grouping

--- src/main/java/com/greendelta/search/wrapper/aggregations/results/AggregationResult.java
+++ src/main/java/com/greendelta/search/wrapper/aggregations/results/AggregationResult.java
@@ -1,6 +1,10 @@
 package com.greendelta.search.wrapper.aggregations.results;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class AggregationResult {
 
@@ -35,4 +39,46 @@ public class AggregationResult {
 		return s + "]}";
 	}
 
+	public void group(String separator) {
+		group(separator, s -> s.split(separator));
+	}
+
+	public void group(String separator, Function<String, String[]> doSplit) {
+		Map<String, AggregationResultEntry> all = new HashMap<>();
+		for (AggregationResultEntry entry : entries) {
+			all.put(entry.key, entry);
+		}
+		List<AggregationResultEntry> topLevel = new ArrayList<>();
+		for (AggregationResultEntry entry : entries) {
+			String[] split = doSplit.apply(entry.key);
+			int depth = 0;
+			String parent = "";
+			while (!all.containsKey(parent) && depth < split.length) {
+				depth++;
+				if (depth == 1 && split.length == 1)
+					continue;
+				parent = join(split, split.length - depth, separator);
+			}
+			if (!parent.isEmpty() && all.containsKey(parent)) {
+				all.get(parent).subEntries.add(entry);
+			} else {
+				topLevel.add(entry);
+			}
+		}
+		entries.clear();
+		entries.addAll(topLevel);
+	}
+
+	private String join(String[] array, int depth, String separator) {
+		String joined = "";
+		for (int index = 0; index < array.length; index++) {
+			if (index >= depth)
+				break;
+			if (!joined.isEmpty() && separator != null) {
+				joined += '/';
+			}
+			joined += array[index];
+		}
+		return joined;
+	}
 }
