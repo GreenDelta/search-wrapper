a271df04a44bfd38457222e1c5db98bdc31bd7a8
greve <greve@greendelta.com>
2018-04-09 16:50:48 +0200

    Added optional field limits

--- src/main/java/com/greendelta/search/wrapper/score/Score.java
+++ src/main/java/com/greendelta/search/wrapper/score/Score.java
@@ -6,13 +6,6 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * Implementing scripts are expected to define the following variables:<br>
- * <br>
- * <b>fieldClass</b>: The calculated class value for the field value<br>
- * <b>valueClass</b>: The calculated class value for the score value<br>
- * <b>classDifference</b>: The difference between the class value of the field
- * value and score value<br>
- * <br>
  * Implementing scripts are expected to define the following methods:<br>
  * <b>substring</b>: String substring(String value, int from, in to)<br>
  * <b>indexOf</b>: int indexOf(String value, String phrase)<br>
@@ -26,37 +19,44 @@ public class Score {
 
 	private List<Case> cases = new ArrayList<>();
 	public final List<Field> fields;
-	public final List<List<Condition>> classes = new ArrayList<>();
+
+	public Score(String field) {
+		this(field, null);
+	}
 
 	public Score(String field, Object value) {
-		this.fields = Collections.singletonList(new Field(field, value));
+		this(new Field(field, value));
+	}
+
+	public Score(String field, Double value, Double lowerLimit, Double upperLimit) {
+		this(new Field(field, value, lowerLimit, upperLimit));
 	}
 
 	public Score(Field... fields) {
 		this.fields = Arrays.asList(fields);
 	}
 
-	public Score addClass(Comparator comparator, Object value) {
-		return addClass("fieldValues[0]", comparator, value);
+	public Score addCase(double weight, Comparator comparator, Object value) {
+		return addCase(weight, "fieldValues[0]", comparator, value);
 	}
 
-	public Score addClass(String field, Comparator comparator, Object value) {
-		return addClass(new Condition(field, comparator, value));
+	public Score addCase(double weight, String field, Comparator comparator, Object value) {
+		return addCase(weight, new Condition(field, comparator, value));
 	}
 
-	public Score addClass(Comparator lowerComparator, Object lowerValue,
+	public Score addCase(double weight, Comparator lowerComparator, Object lowerValue,
 			Comparator upperComparator, Object upperValue) {
-		return addClass("fieldValues[0]", lowerComparator, lowerValue, upperComparator, upperValue);
+		return addCase(weight, "fieldValues[0]", lowerComparator, lowerValue, upperComparator, upperValue);
 	}
 
-	public Score addClass(String field, Comparator lowerComparator, Object lowerValue,
+	public Score addCase(double weight, String field, Comparator lowerComparator, Object lowerValue,
 			Comparator upperComparator, Object upperValue) {
-		return addClass(new Condition(field, lowerComparator, lowerValue),
+		return addCase(weight, new Condition(field, lowerComparator, lowerValue),
 				new Condition(field, upperComparator, upperValue));
 	}
 
-	public Score addClass(Condition... conditions) {
-		classes.add(Arrays.asList(conditions));
+	public Score addCase(double weight, Condition... conditions) {
+		cases.add(new Case(weight, conditions));
 		return this;
 	}
 
@@ -67,17 +67,17 @@ public class Score {
 		return this;
 	}
 
-	public Score addCase(double weight, String field, Comparator comparator, Object value) {
-		return addCase(weight, new Condition(field, comparator, value));
-	}
-
-	public Score addCase(double weight, Condition... conditions) {
-		this.cases.add(new Case(weight, conditions));
-		return this;
-	}
-
 	public Case[] getCases() {
 		return cases.toArray(new Case[cases.size()]);
+	}
+
+	public double getDefaultWeight() {
+		if (cases.isEmpty())
+			return 1;
+		Case lastCase = cases.get(cases.size() - 1);
+		if (lastCase.conditions.isEmpty())
+			return lastCase.weight;
+		return 1;
 	}
 
 }
